d784950c46e56108f1067aa0a14ce31b98ed96d1
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2024-03-26 20:54:23 -0300

    adição do zero

--- hora-atual/script.js
+++ hora-atual/script.js
@@ -6,6 +6,21 @@ function atualizarhora(){
     let minuto = d.getMinutes()
     let hora = d.getHours()
 
+    if(segundo < 10){
+        segundo = '0' + segundo
+    } else{
+        segundo = segundo
+    }
+    if (minuto < 10){
+        minuto = '0' + minuto
+    } else {
+        minuto = minuto
+    }
+    if (hora < 10){
+        hora = '0' + hora
+    } else {
+        hora = hora
+    }
     
     msg.textContent = hora + ':' + minuto + ':' + segundo
 
